bcc2a8c3be4a2790cf147b876c3d86aaaffced72
Reyes Ruiz <reyes@yahoo-inc.com>
2015-08-09 22:25:06 -0700

    Implementing code to calculate if it is yard waste or recycle per week number. Also adding Spanish translation

--- app/src/main/java/com/digitalruiz/cityoftracygarbagecollectionschedule/MainActivity.java
+++ app/src/main/java/com/digitalruiz/cityoftracygarbagecollectionschedule/MainActivity.java
@@ -18,27 +18,51 @@ public class MainActivity extends Activity {
 
         Time timenow  = new Time();
         timenow.setToNow();
-        Calendar c = Calendar.getInstance();
-        c.setTimeInMillis(timenow.toMillis(false));
-        int weekOfYear = c.get(Calendar.WEEK_OF_YEAR);
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeInMillis(timenow.toMillis(false));
+        int weekOfYear = cal.get(Calendar.WEEK_OF_YEAR);
+        int daynumber = cal.get(Calendar.DAY_OF_WEEK);
         boolean even;
+        String message;
+        if (daynumber == 1){
+            message = getString(R.string.dayofputtingbinsout);
+
+        }
+        else if (daynumber == 2){
+            message = getString(R.string.dayofpickup);
+
+        }
+        else {
+            message = getString(R.string.nextpickup);
+            weekOfYear = weekOfYear+1;
+
+        }
         if ((weekOfYear % 2 ) == 0) {
             even = true;
         }
         else {
             even = false;
         }
-        String recycleoryard;
+
+//Using yard as even.
+        String yardorecycle;
         if ((even == true)){
-            recycleoryard = "Yard Waste";
+            yardorecycle = getString(R.string.yard);
         }
         else {
-            recycleoryard = "Recycle";
+            yardorecycle = getString(R.string.recycle);
         }
+        String turn = getString(R.string.turn);
         String date = DateUtils.formatDateTime(this, timenow.toMillis(false), DateUtils.FORMAT_SHOW_WEEKDAY|DateUtils.FORMAT_SHOW_DATE|DateUtils.FORMAT_SHOW_YEAR);
-        String todaysformatted = "Todays Date is: " + date + " which is in Week number: " + weekOfYear + " so this weeks is " + recycleoryard + " turn";
+        String todaysformatted = getString(R.string.today) +" " + date;
         TextView todaysdate = (TextView) findViewById(R.id.tv_todays_date);
         todaysdate.setText(todaysformatted);
+        TextView messageview = (TextView) findViewById(R.id.tv_message);
+        messageview.setText(message);
+        TextView turnview = (TextView) findViewById(R.id.tv_turn);
+        turnview.setText(turn);
+        TextView recycleoryardview = (TextView) findViewById(R.id.tv_recycleoryard);
+        recycleoryardview.setText(yardorecycle);
     }
 
 
