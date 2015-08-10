560212f7ebe161856506f3ccc7635f3bf29cc411
Reyes Ruiz <reyes@yahoo-inc.com>
2015-08-09 17:30:22 -0700

    Adding a way to calculate if it is yard or recycle turn, right now it is only meant for East Side of Tracy Blvd

--- app/src/main/java/com/digitalruiz/cityoftracygarbagecollectionschedule/MainActivity.java
+++ app/src/main/java/com/digitalruiz/cityoftracygarbagecollectionschedule/MainActivity.java
@@ -6,6 +6,7 @@ import android.text.format.DateUtils;
 import android.text.format.Time;
 import android.widget.TextView;
 
+import java.util.Calendar;
 
 
 public class MainActivity extends Activity {
@@ -17,8 +18,25 @@ public class MainActivity extends Activity {
 
         Time timenow  = new Time();
         timenow.setToNow();
+        Calendar c = Calendar.getInstance();
+        c.setTimeInMillis(timenow.toMillis(false));
+        int weekOfYear = c.get(Calendar.WEEK_OF_YEAR);
+        boolean even;
+        if ((weekOfYear % 2 ) == 0) {
+            even = true;
+        }
+        else {
+            even = false;
+        }
+        String recycleoryard;
+        if ((even == true)){
+            recycleoryard = "Yard Waste";
+        }
+        else {
+            recycleoryard = "Recycle";
+        }
         String date = DateUtils.formatDateTime(this, timenow.toMillis(false), DateUtils.FORMAT_SHOW_WEEKDAY|DateUtils.FORMAT_SHOW_DATE|DateUtils.FORMAT_SHOW_YEAR);
-        String todaysformatted = "Todays Date is: " + date;
+        String todaysformatted = "Todays Date is: " + date + " which is in Week number: " + weekOfYear + " so this weeks is " + recycleoryard + " turn";
         TextView todaysdate = (TextView) findViewById(R.id.tv_todays_date);
         todaysdate.setText(todaysformatted);
     }
