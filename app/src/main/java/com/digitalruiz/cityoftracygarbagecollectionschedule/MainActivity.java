5663b19981ac4faf5713ea3cf243e40a03a6014d
Reyes Ruiz <reyes@yahoo-inc.com>
2015-08-12 16:59:07 -0700

    Re-arranging the layout view, and adding images

--- app/src/main/java/com/digitalruiz/cityoftracygarbagecollectionschedule/MainActivity.java
+++ app/src/main/java/com/digitalruiz/cityoftracygarbagecollectionschedule/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.text.format.DateUtils;
 import android.text.format.Time;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -15,7 +16,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        new Waste(MainActivity.this);
+       // new Waste(MainActivity.this);
         setContentView(R.layout.activity_main);
 
         Time timenow  = new Time();
@@ -58,21 +59,27 @@ public class MainActivity extends Activity {
 //Using recycle as even.
 
         String yardorecycle;
+        String descyardorrecycle;
+        Integer imageyardorrecycle;
 
         if ((even == true)){
             Waste waste = Waste.wastes[0];
             yardorecycle = getString(waste.getName());
-            //yardorecycle = getString(R.string.recycle);
+            descyardorrecycle = getString(waste.getDescription());
+            imageyardorrecycle = waste.getImageID();
         }
         else {
             Waste waste = Waste.wastes[1];
             yardorecycle = getString(waste.getName());
-          //  yardorecycle = getString(R.string.yard);
+            descyardorrecycle = getString(waste.getDescription());
+            imageyardorrecycle = waste.getImageID();
         }
         Waste waste = Waste.wastes[2];
+        String descGarbage = getString(waste.getDescription());
+        Integer imagegarbage = waste.getImageID();
         String garbage = getString(waste.getName());
-        String and = getString(R.string.and);
-        yardorecycle = garbage + " " + and + " " + yardorecycle;
+        //String and = getString(R.string.and);
+        //yardorecycle = garbage + " " + and + " " + yardorecycle;
         String turn = getString(R.string.turn);
         String date = DateUtils.formatDateTime(this, timenow.toMillis(false), DateUtils.FORMAT_SHOW_WEEKDAY|DateUtils.FORMAT_SHOW_DATE|DateUtils.FORMAT_SHOW_YEAR);
         String todaysformatted = getString(R.string.today) +" " + date;
@@ -84,6 +91,16 @@ public class MainActivity extends Activity {
         turnview.setText(turn);
         TextView recycleoryardview = (TextView) findViewById(R.id.tv_recycleoryard);
         recycleoryardview.setText(yardorecycle);
+        TextView garbabeview = (TextView) findViewById(R.id.tv_garbage);
+        garbabeview.setText(getString(R.string.garbage));
+        TextView descGarbageview = (TextView) findViewById(R.id.tv_descGarbage);
+        TextView descRecycleorYardview = (TextView) findViewById(R.id.tv_descRecycleorYard);
+        descGarbageview.setText(descGarbage);
+        descRecycleorYardview.setText(descyardorrecycle);
+        ImageView garbageimageview = (ImageView) findViewById(R.id.iv_garbage);
+        garbageimageview.setImageResource(imagegarbage);
+        ImageView yardorrecycleimageview = (ImageView) findViewById(R.id.iv_yardorrecycle);
+        yardorrecycleimageview.setImageResource(imageyardorrecycle);
     }
 
 
--- app/src/main/java/com/digitalruiz/cityoftracygarbagecollectionschedule/Waste.java
+++ app/src/main/java/com/digitalruiz/cityoftracygarbagecollectionschedule/Waste.java
@@ -10,31 +10,33 @@ import android.content.res.Resources;
 public class Waste {
     private int name;
     private int description;
+    private int imageid;
 
 
 
-    Context mContext;
-    public Waste(Context context)
-    {
-        mContext = context;
-    }
-    private static final int recycle = R.string.recycle;
-    private static final int yard = R.string.yard;
-    private static final int garbage = R.string.garbage;
-    private static final int descRecycle = R.string.descRecycle;
-    private static final int descYard = R.string.descYard;
-    private static final int descGarbage = R.string.descGarbage;
+    //Context mContext;
+    //public Waste(Context context)
+   // {
+       // mContext = context;
+    //}
+    //private static final int recycle = R.string.recycle;
+    //private static final int yard = R.string.yard;
+    //private static final int garbage = R.string.garbage;
+    //private static final int descRecycle = R.string.descRecycle;
+    //private static final int descYard = R.string.descYard;
+    //private static final int descGarbage = R.string.descGarbage;
 
 
     public static final Waste[] wastes = {
-            new Waste(recycle, descRecycle),
-            new Waste(yard, descYard),
-            new Waste(garbage, descGarbage),
+            new Waste(R.string.recycle, R.string.descRecycle, R.drawable.recycling_cart),
+            new Waste(R.string.yard, R.string.descYard, R.drawable.yard_waste_cart),
+            new Waste(R.string.garbage, R.string.descGarbage, R.drawable.garbage_cart),
     };
 
-    private Waste(Integer name, Integer description) {
+    private Waste(Integer name, Integer description, Integer imageid) {
         this.name = name;
         this.description = description;
+        this.imageid = imageid;
     }
 
     public Integer getName() {
@@ -45,6 +47,9 @@ public class Waste {
         return description;
     }
 
+    public Integer getImageID(){
+        return imageid;
+    }
 
 
 
