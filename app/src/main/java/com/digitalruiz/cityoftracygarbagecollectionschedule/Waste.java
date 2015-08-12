7af7f1f02f46be79330e17fd4efdf3623fc7d743
Reyes Ruiz <reyes@yahoo-inc.com>
2015-08-11 22:39:41 -0700

    Adding description to the Waste class

--- app/src/main/java/com/digitalruiz/cityoftracygarbagecollectionschedule/Waste.java
+++ app/src/main/java/com/digitalruiz/cityoftracygarbagecollectionschedule/Waste.java
@@ -9,8 +9,8 @@ import android.content.res.Resources;
  */
 public class Waste {
     private int name;
-   // private String description;
-    //private String recycle;
+    private int description;
+
 
 
     Context mContext;
@@ -21,21 +21,28 @@ public class Waste {
     private static final int recycle = R.string.recycle;
     private static final int yard = R.string.yard;
     private static final int garbage = R.string.garbage;
+    private static final int descRecycle = R.string.descRecycle;
+    private static final int descYard = R.string.descYard;
+    private static final int descGarbage = R.string.descGarbage;
 
 
     public static final Waste[] wastes = {
-            new Waste(recycle),
-            new Waste(yard),
-            new Waste(garbage)
+            new Waste(recycle, descRecycle),
+            new Waste(yard, descYard),
+            new Waste(garbage, descGarbage),
     };
 
-    private Waste(Integer name) {
+    private Waste(Integer name, Integer description) {
         this.name = name;
-
+        this.description = description;
     }
 
     public Integer getName() {
         return name;
+    }
+
+    public Integer getDescription() {
+        return description;
     }
 
 
