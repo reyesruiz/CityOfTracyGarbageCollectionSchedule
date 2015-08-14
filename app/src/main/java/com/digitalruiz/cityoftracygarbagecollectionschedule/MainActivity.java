b2e4ba3c2ba8e17470f8e58bbd63bee45b71881d
Reyes Ruiz <reyes@yahoo-inc.com>
2015-08-13 22:34:21 -0700

    Orginizing the code

--- app/src/main/java/com/digitalruiz/cityoftracygarbagecollectionschedule/MainActivity.java
+++ app/src/main/java/com/digitalruiz/cityoftracygarbagecollectionschedule/MainActivity.java
@@ -16,18 +16,37 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       // new Waste(MainActivity.this);
         setContentView(R.layout.activity_main);
+        //Declaring references to access the views.
+        TextView todaysdate = (TextView) findViewById(R.id.tv_todays_date);
+        TextView messageview = (TextView) findViewById(R.id.tv_message);
+        TextView turnview = (TextView) findViewById(R.id.tv_turn);
+        TextView recycleoryardview = (TextView) findViewById(R.id.tv_recycleoryard);
+        TextView garbabeview = (TextView) findViewById(R.id.tv_garbage);
+        TextView descGarbageview = (TextView) findViewById(R.id.tv_descGarbage);
+        TextView descRecycleorYardview = (TextView) findViewById(R.id.tv_descRecycleorYard);
+        ImageView garbageimageview = (ImageView) findViewById(R.id.iv_garbage);
+        ImageView yardorrecycleimageview = (ImageView) findViewById(R.id.iv_yardorrecycle);
 
+        //Time and Calendar
         Time timenow  = new Time();
-        timenow.setToNow();
         Calendar cal = Calendar.getInstance();
+
+        //set time and calendar to now.
+        timenow.setToNow();
         cal.setTimeInMillis(timenow.toMillis(false));
+
+        //declaring Variables
         int weekOfYear = cal.get(Calendar.WEEK_OF_YEAR);
         int daynumber = cal.get(Calendar.DAY_OF_WEEK);
         String sideoftracyblvd = "east";
         boolean even;
         String message;
+        String yardorecycle;
+        String descyardorrecycle;
+        int imageyardorrecycle;
+
+        // Side of tracy blvd, this will be set via Settings in later version
         if (sideoftracyblvd == "east") {
             //Nothing needs to be done, since east will be used as default
         }
@@ -36,6 +55,8 @@ public class MainActivity extends Activity {
             //Adding 1 to the weeofyear to make it opposite as east,
             weekOfYear = weekOfYear + 1;
         }
+
+        //Now using Monday as a pickup day, this will be set in Settings page in later version.
         if (daynumber == 1){
             message = getString(R.string.dayofputtingbinsout);
 
@@ -49,6 +70,9 @@ public class MainActivity extends Activity {
             weekOfYear = weekOfYear+1;
 
         }
+
+
+        //Figuring out if it is even or odd week number
         if ((weekOfYear % 2 ) == 0) {
             even = true;
         }
@@ -56,12 +80,7 @@ public class MainActivity extends Activity {
             even = false;
         }
 
-//Using recycle as even.
-
-        String yardorecycle;
-        String descyardorrecycle;
-        Integer imageyardorrecycle;
-
+        //Using recycle as even.
         if ((even == true)){
             Waste waste = Waste.wastes[0];
             yardorecycle = getString(waste.getName());
@@ -74,32 +93,27 @@ public class MainActivity extends Activity {
             descyardorrecycle = getString(waste.getDescription());
             imageyardorrecycle = waste.getImageID();
         }
+
+        //Garbage is every week, so no need to calculate anything.
         Waste waste = Waste.wastes[2];
         String descGarbage = getString(waste.getDescription());
         Integer imagegarbage = waste.getImageID();
         String garbage = getString(waste.getName());
-        //String and = getString(R.string.and);
-        //yardorecycle = garbage + " " + and + " " + yardorecycle;
+
+        //Getting and setting the strings of the messages to be displayed
         String turn = getString(R.string.turn);
-        String date = DateUtils.formatDateTime(this, timenow.toMillis(false), DateUtils.FORMAT_SHOW_WEEKDAY|DateUtils.FORMAT_SHOW_DATE|DateUtils.FORMAT_SHOW_YEAR);
+        String date = DateUtils.formatDateTime(this, timenow.toMillis(false), DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR);
         String todaysformatted = getString(R.string.today) +" " + date;
-        TextView todaysdate = (TextView) findViewById(R.id.tv_todays_date);
+
+        //Setting text and images to the display.
         todaysdate.setText(todaysformatted);
-        TextView messageview = (TextView) findViewById(R.id.tv_message);
         messageview.setText(message);
-        TextView turnview = (TextView) findViewById(R.id.tv_turn);
         turnview.setText(turn);
-        TextView recycleoryardview = (TextView) findViewById(R.id.tv_recycleoryard);
         recycleoryardview.setText(yardorecycle);
-        TextView garbabeview = (TextView) findViewById(R.id.tv_garbage);
         garbabeview.setText(getString(R.string.garbage));
-        TextView descGarbageview = (TextView) findViewById(R.id.tv_descGarbage);
-        TextView descRecycleorYardview = (TextView) findViewById(R.id.tv_descRecycleorYard);
         descGarbageview.setText(descGarbage);
         descRecycleorYardview.setText(descyardorrecycle);
-        ImageView garbageimageview = (ImageView) findViewById(R.id.iv_garbage);
         garbageimageview.setImageResource(imagegarbage);
-        ImageView yardorrecycleimageview = (ImageView) findViewById(R.id.iv_yardorrecycle);
         yardorrecycleimageview.setImageResource(imageyardorrecycle);
     }
 
